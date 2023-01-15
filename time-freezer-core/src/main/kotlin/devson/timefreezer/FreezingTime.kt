a8dd765043cc898d457cb6df7f2436c3d9e041ea
devson <sonyc5720@gmail.com>
2023-01-15 12:54:54 +0900

    refactor: make FreezingTime class to internal class

--- time-freezer-core/src/main/kotlin/devson/timefreezer/FreezingTime.kt
+++ time-freezer-core/src/main/kotlin/devson/timefreezer/FreezingTime.kt
@@ -1,18 +1,18 @@
 package devson.timefreezer
 
-sealed class FreezingTime<DATE, DATE_TIME> {
+internal sealed class FreezingTime<DATE, DATE_TIME> {
 
     open val isLocalDate: Boolean = false
     open val isLocalDateTime: Boolean = false
 
-    data class LocalDate(
+    internal data class LocalDate(
         val value: java.time.LocalDate
     ) : FreezingTime<java.time.LocalDate, Nothing>() {
 
         override val isLocalDate: Boolean = true
     }
 
-    data class LocalDateTime(
+    internal data class LocalDateTime(
         val value: java.time.LocalDateTime
     ) : FreezingTime<Nothing, java.time.LocalDateTime>() {
 
