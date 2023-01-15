49fbc347deff0e9d14b02e1d90c5d95be4f2c7d7
devson <sonyc5720@gmail.com>
2023-01-15 11:41:02 +0900

    misc: add Coupon example

---
+++ time-freezer-example/src/test/kotlin/devson/timefreezer/example/CouponExample.kt
@@ -0,0 +1,49 @@
+package devson.timefreezer.example
+
+import devson.timefreezer.freeze
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+internal class CouponExample : FreeSpec({
+    "Coupon example" {
+        val sut = Coupon(
+            canUseFrom = LocalDate.of(2023, 1, 1),
+            expiresAt = LocalDateTime.of(2023, 1, 3, 22, 0, 0),
+        )
+
+        // before date can use the coupon
+        freeze(LocalDate.of(2022, 12, 31)) {
+            sut.isAvailable().shouldBeFalse()
+            sut.isAfterCanUseFrom().shouldBeFalse()
+            sut.isExpired().shouldBeFalse()
+        }
+
+        // during date can use the coupon
+        freeze(LocalDateTime.of(2023, 1, 1, 0, 0, 0)) {
+            sut.isAvailable().shouldBeTrue()
+            sut.isAfterCanUseFrom().shouldBeTrue()
+            sut.isExpired().shouldBeFalse()
+        }
+
+        // after coupon expired
+        freeze(LocalDateTime.of(2023, 1, 3, 23, 0, 1)) {
+            sut.isAvailable().shouldBeFalse()
+            sut.isAfterCanUseFrom().shouldBeTrue()
+            sut.isExpired().shouldBeTrue()
+        }
+    }
+})
+
+private data class Coupon(
+    val canUseFrom: LocalDate,
+    val expiresAt: LocalDateTime,
+) {
+    fun isAvailable(): Boolean = (this.isAfterCanUseFrom()) && this.isExpired().not()
+
+    fun isAfterCanUseFrom(): Boolean = (this.canUseFrom <= LocalDate.now())
+
+    fun isExpired(): Boolean = (LocalDateTime.now() > this.expiresAt)
+}
